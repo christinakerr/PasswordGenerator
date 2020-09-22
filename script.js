84e5a22efdcdeb61a2fbbd16620f93427895c572
Christi Kerr <christinakerr394@gmail.com>
2020-09-22 16:52:03 -0500

    Added validation to ensure password length is from 8 to 128

--- script.js
+++ script.js
@@ -12,14 +12,16 @@ function writePassword() {
 
 function generatePassword() {
   var length = prompt("How many characters would you like your password to be?");
+  if ((length < 8) || (length > 128)) {
+    alert("Please select a number from 8 to 128.")
+    generatePassword();
+  } else {
+    var lowercase = confirm("Press OK to include lowercase letters.");
+    var uppercase = confirm("Press OK to include uppercase letters.");
+    var numeric = confirm("Press OK to include numbers.");
+    var specialCharacters = confirm("Press OK to include special characters.");
   // validation 
-
-  var lowercase = confirm("Press OK to include lowercase letters.");
-  var uppercase = confirm("Press OK to include uppercase letters.");
-  var numeric = confirm("Press OK to include numbers.");
-  var specialCharacters = confirm("Press OK to include special characters.");
-  // validation 
-  console.log(length, lowercase, uppercase, numeric, specialCharacters)
+  }
 }
 
 // Add event listener to generate button
